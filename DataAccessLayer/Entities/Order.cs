b3da39afb3193421990f19763d3c1c7bb690e6d0
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-07-01 10:22:19 +0100

    feat: add mongoDb entities for Order & OrderItem

---
+++ DataAccessLayer/Entities/Order.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid _id { get; set;  }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid OrderID { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid UserID { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        public DateTime OrderDate { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal TotalAmount { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ DataAccessLayer/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class OrderItem
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid _id { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid ProductID { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal UnitPrice { get; set; }
+
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int Quantity { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal TotalPrice { get; set; }
+    }
+}
