eb1ffc6e7a92f1f4251815ea4ecac951b3c5b311
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-07-13 13:42:13 +0100

    chore: refactor failure to decorate OrderResponse's with user info, to instead return fault data instead of throwing exception As it is non-essential functionality in the response, only extra info, so still allow users to retrieve their list of orders

--- BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
+++ BusinessLogicLayer/HttpClients/UsersMicroserviceClient.cs
@@ -20,9 +20,20 @@ namespace BusinessLogicLayer.HttpClients
         public async Task<UserDTO> GetUserByUserID(Guid userID)
         {
             var response = await _httpClient.GetAsync($"api/users/{userID}");
-            
-            if(response.StatusCode == System.Net.HttpStatusCode.NotFound) return null; // if the user is not found, return null
-            else if (!response.IsSuccessStatusCode) throw new HttpRequestException($"Error retrieving user with ID {userID}: {response.ReasonPhrase}"); // if any other error occurs, throw an exception
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound) return null; // if the user is not found, return null
+            //else if (!response.IsSuccessStatusCode) throw new HttpRequestException($"Error retrieving user with ID {userID}: {response.ReasonPhrase}"); // if any other error occurs, throw an exception
+            else if (!response.IsSuccessStatusCode)
+            {
+                // if any other error occurs, return Fault Data instead of an exception so that the application can continue to run and display an error message to the user
+                return new UserDTO
+                (
+                    Guid.Empty,
+                    "Error retrieving user",
+                    "Error retrieving user",
+                    "Error retrieving user"
+                );
+            }
 
             var user = await response.Content.ReadFromJsonAsync<UserDTO>();
             return user;
