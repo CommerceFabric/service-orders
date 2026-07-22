f442fba2fe174df621ffdd2e34e769733397b833
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-07-22 13:26:08 +0100

    fix: make api gateway accessible via angular frontend - add cors policy - modify ocelot.json so all upstreampaths end in a slash

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -4,13 +4,25 @@ using Ocelot.Middleware;
 #region Configuring Builder
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional:false, reloadOnChange:true); // Load Ocelot configuration from ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true); // Load Ocelot configuration from ocelot.json
 builder.Services.AddOcelot(); // Add Ocelot services to the DI container
+
+// add CORS policy to allow requests from the Angular frontend
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 #endregion
 
 var app = builder.Build();
 
 #region Configuring App
+app.UseCors();
 await app.UseOcelot(); // Use Ocelot middleware to handle requests
 #endregion
 
