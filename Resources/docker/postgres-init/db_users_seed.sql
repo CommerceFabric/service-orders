3d8adef7ff91fdc5193dcff551b48a12fbfa8857
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-08-07 17:57:16 +0100

    chore: update seeding for users db

--- Resources/docker/postgres-init/db_users_seed.sql
+++ Resources/docker/postgres-init/db_users_seed.sql
@@ -1,17 +1,21 @@
 -- CREATE Database userService; -- not needed as this is done in the docker-compose file
 
 CREATE TABLE public.users (
-    userid UUID PRIMARY KEY,
-    personname VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    gender VARCHAR(50) NOT NULL,
-    CONSTRAINT email_unique UNIQUE (email)
+    UserID UUID PRIMARY KEY,
+    Gender VARCHAR(50) NOT NULL,
+    Bio VARCHAR(500)
 );
 
-
 -- Sample data for insertion
-INSERT INTO public.users (userid, email, personname, gender, password)
-VALUES 
-('c32f8b42-60e6-4c02-90a7-9143ab37189f', 'test1@example.com', 'John Doe', 'Male', 'password1'),
-('8ff22c7d-18c7-4ef0-a0ac-988ecb2ac7f5', 'test2@example.com', 'Jane Smith', 'Female', 'password2');
+INSERT INTO public.users (UserID, Gender, Bio)
+VALUES
+(
+    'c32f8b42-60e6-4c02-90a7-9143ab37189f',
+    'Male',
+    'John Doe is a sample user used for testing.'
+),
+(
+    '8ff22c7d-18c7-4ef0-a0ac-988ecb2ac7f5',
+    'Female',
+    'Jane Doe is a sample user used for testing.'
+);a
